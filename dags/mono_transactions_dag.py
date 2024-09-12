5986b48e83d7e3f457fa2c55b995a2d5944d1211
Gurlenko <136372389+Gurlenko@users.noreply.github.com>
2024-09-12 13:12:06 +0300

    convert_to_unix and get_data_from_api tasks

--- dags/mono_transactions_dag.py
+++ dags/mono_transactions_dag.py
@@ -1,31 +1,37 @@
 from airflow import DAG
+from airflow.models import Variable
 from airflow.operators.python import PythonOperator
-from airflow.operators import DummyOperator
-from airflow.utils.dates import days_ago, datetime, timedelta
-from scripts.mono_transactions import get_data_from_api
-from dotenv import load_dotenv
+from airflow.operators.dummy import DummyOperator
+from airflow.utils.dates import days_ago, datetime
+from scripts.mono_transactions import get_data_from_api, from_json_to_dataframe, insert_in_postgres
+from datetime import timedelta  
 import os
+import pendulum 
 
-dotenv_path = '/Users/admin/Desktop/Mono/.env'
-load_dotenv(dotenv_path)
+dotenv_path = '/Users/admin/Desktop/Mono/dags/.env'
 
+xtoken = Variable.get("XToken")
 
-xtoken = os.getenv('XToken')
 
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': days_ago(1),
-    'retries': 1,
+    'retries': 10,
+    'retry_delay': timedelta(minutes=1),  
 }
+
+
+def convert_to_unix(ds, **kwargs):
+    from_date = pendulum.parse(ds).start_of('day').int_timestamp
+    to_date = pendulum.parse(ds).end_of('day').int_timestamp
+    return from_date, to_date
 
 with DAG(
     dag_id='Mono_transactions',
     default_args=default_args,
-    description='Простий DAG з Dummy та Python операторами',
-    schedule_interval='@daily',
+    description='Отримання даних по операціям Монобанку',
     schedule_interval='0 0 * * *',
-    start_date=datetime(2019, 6, 10),
+    start_date=datetime(2024, 9, 1),
     catchup=True,
 ) as dag:
 
@@ -33,34 +39,35 @@ with DAG(
         task_id='start'
     )
 
-
     end = DummyOperator(
         task_id='end'
     )
 
-    task1 = PythonOperator(
+    get_data = PythonOperator(
         task_id='get_json_transactions',
         python_callable=get_data_from_api,
         op_kwargs={
-            'token': xtoken,
+            'xtoken': xtoken,
             'account': 0,
-            'from_date': '{{ ds }}',
-            'to_date': '{{ ds }}' 
+            'from_date': '{{ task_instance.xcom_pull(task_ids="convert_dates")[0] }}',
+            'to_date': '{{ task_instance.xcom_pull(task_ids="convert_dates")[1] }}'
         }
     )
 
-    task2 = PythonOperator(
+    convert_dates = PythonOperator(
+        task_id='convert_dates',
+        python_callable=convert_to_unix,
+        provide_context=True
+    )
+
+    transform_data = PythonOperator(
         task_id='my_second_task',
-        python_callable=my_second_function
+        python_callable=from_json_to_dataframe
     )
 
-    task3 = PythonOperator(
+    insert_data = PythonOperator(
         task_id='my_third_task',
-        python_callable=my_third_function
+        python_callable=insert_in_postgres
     )
 
-
-
-
-    start >> [task1, task2] >> task3 >> end
-
+    start >> convert_dates >> get_data >> transform_data >> insert_data >> end
--- scripts/mono_transactions.py
+++ scripts/mono_transactions.py
@@ -1,16 +1,26 @@
 import requests
 
 
-def get_data_from_api(account: int, from_date: int, to_date: int, token: str) -> dict:
+def get_data_from_api(account: int, from_date: int, to_date: int, xtoken: str) -> dict:
     url = f'https://api.monobank.ua/personal/statement/{account}/{from_date}/{to_date}'
-
+    print(xtoken)
+    print(url)
     headers = {
         'Content-Type': 'application/json',
-        'X-Token': token 
+        'X-Token': xtoken 
     }
 
     response = requests.get(url, headers=headers)
     response.raise_for_status()
-
+    print(response.status_code)
+    print(response.json())
     return response.json()
 
+
+
+def from_json_to_dataframe():
+    pass
+
+
+def insert_in_postgres():
+    pass
